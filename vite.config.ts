177c355291a5292630abd496a5bc4fb6c75c50e9
Viktoriia Makusheva <miss.black.flower@gmail.com>
2023-03-18 18:48:36 +0200

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: '@ctux/lib',
+      name: 'mbf-leetcode',
       fileName: 'lib',
     },
   },
